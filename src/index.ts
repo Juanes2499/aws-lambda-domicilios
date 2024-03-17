ecd948d7312c48e93a8fa15b377249d708450cd0
Juan Esteban Nichoy Larranaga <jenichoyl@bancofalabella.com.co>
2024-03-17 18:15:55 -0500

    Creating repository for AWS Lambda Domicilios

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { SQSEvent, Handler } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { InputOrdenCompra } from "./models/InputOrdenCompra";
+import moment from "moment-timezone";
+
+const dynamoDBClient = new DynamoDBClient({ region: "us-east-1" });
+const ddbDocClient = DynamoDBDocumentClient.from(dynamoDBClient);
+
+export const handler: Handler = async (event: SQSEvent) => {
+
+  for (const record of event.Records) {
+    const ordenCompraData = JSON.parse(record.body) as InputOrdenCompra;
+
+    console.info(`Starting Domicilios - Orden Compra ID: ${ordenCompraData.ordenCompraId}`);
+
+    try {
+
+      if (!ordenCompraData.data.mockDLQ?.process) {
+        throw new Error("MockDLQ process is in false");
+      }
+
+      const timezone = 'America/Bogota';
+      const currentTime = moment().tz(timezone).format();
+
+      const item = {
+        TableName: "DynamoDb-Orden-Compra",
+        Key: { ordenCompraId: ordenCompraData.ordenCompraId },
+        UpdateExpression: "SET stages = list_append(stages, :newElement), updatedAt = :updatedAt",
+        ExpressionAttributeValues: {
+          ":newElement": ["DOMICILIO PROCESSED"],
+          ":updatedAt": currentTime.toString(),
+        },
+      };
+
+      await ddbDocClient.send(new UpdateCommand(item));
+
+      console.info(`Domicilios - Orden Compra ID: ${ordenCompraData.ordenCompraId} Processed`);
+    } catch (errDynamoDb) {
+      console.error("Error processing record", record, errDynamoDb);
+      throw new Error(`Errors occurred processing records: ${errDynamoDb}`);
+    }
+  }
+};
